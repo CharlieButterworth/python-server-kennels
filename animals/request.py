6225d5cca2c3c2615e7318e1f29bc033a792c686
Charles Butterworth <charlessbutterworth@gmail.com>
2021-01-20 16:03:21 -0600

    finished ch13

--- animals/request.py
+++ animals/request.py
@@ -144,44 +144,31 @@ def delete_animal(id):
         WHERE id = ?
         """, (id, ))
 
-# def create_animal(animal):
-#     # Get the id value of the last animal in the list
-#     max_id = ANIMALS[-1]["id"]
 
-#     # Add 1 to whatever that number is
-#     new_id = max_id + 1
+def update_animal(id, new_animal):
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
 
-#     # Add an `id` property to the animal dictionary
-#     animal["id"] = new_id
+        db_cursor.execute("""
+        UPDATE Animal
+            SET
+                name = ?,
+                breed = ?,
+                status = ?,
+                location_id = ?,
+                customer_id = ?
+        WHERE id = ?
+        """, (new_animal['name'], new_animal['breed'],
+              new_animal['status'], new_animal['location_id'],
+              new_animal['customer_id'], id, ))
 
-#     # Add the animal dictionary to the list
-#     ANIMALS.append(animal)
+        # Were any rows affected?
+        # Did the client send an `id` that exists?
+        rows_affected = db_cursor.rowcount
 
-#     # Return the dictionary with `id` property added
-#     return animal
-
-
-# def delete_animal(id):
-#     # Initial -1 value for animal index, in case one isn't found
-#     animal_index = -1
-
-#     # Iterate the ANIMALS list, but use enumerate() so that you
-#     # can access the index value of each item
-#     for index, animal in enumerate(ANIMALS):
-#         if animal["id"] == id:
-#             # Found the animal. Store the current index.
-#             animal_index = index
-
-#     # If the animal was found, use pop(int) to remove it from list
-#     if animal_index >= 0:
-#         ANIMALS.pop(animal_index)
-
-
-# def update_animal(id, new_animal):
-#     # Iterate the ANIMALS list, but use enumerate() so that
-#     # you can access the index value of each item.
-#     for index, animal in enumerate(ANIMALS):
-#         if animal["id"] == id:
-#             # Found the animal. Update the value.
-#             ANIMALS[index] = new_animal
-#             break
+    if rows_affected == 0:
+        # Forces 404 response by main module
+        return False
+    else:
+        # Forces 204 response by main module
+        return True
