c6313e1bd5c56340d78feea845b9805de8c1e6f3
Charles Butterworth <charlessbutterworth@gmail.com>
2021-01-21 14:28:04 -0600

    finished ch14

--- employees/request.py
+++ employees/request.py
@@ -1,6 +1,7 @@
 import sqlite3
 import json
 from models import Employee
+from models import Location
 
 
 def get_all_employees():
@@ -14,8 +15,12 @@ def get_all_employees():
             e.id,
             e.name,
             e.address,
-            e.location_id
-        FROM employee e 
+            e.location_id,
+            l.name,
+            l.address
+        FROM employee e
+        JOIN Location l 
+            ON l.id = e.location_id
         """)
 
         employees = []
@@ -26,6 +31,11 @@ def get_all_employees():
 
             employee = Employee(row['id'], row['name'], row['address'],
                                 row['location_id'])
+
+            location = Location(
+                row['location_id'], row['name'], row['address'])
+
+            employee.location = location.__dict__
 
             employees.append(employee.__dict__)
 
