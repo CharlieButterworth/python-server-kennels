1a41a29f17669e986bea3a650de1efc0c52adbfe
Charles Butterworth <charlessbutterworth@gmail.com>
2021-01-25 09:47:15 -0600

    finished ch15

--- animals/request.py
+++ animals/request.py
@@ -199,3 +199,29 @@ def update_animal(id, new_animal):
     else:
         # Forces 204 response by main module
         return True
+
+
+def create_animal(new_animal):
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        INSERT INTO Animal
+            ( name, breed, status, location_id, customer_id )
+        VALUES
+            ( ?, ?, ?, ?, ?);
+        """, (new_animal['name'], new_animal['species'],
+              new_animal['status'], new_animal['location_id'],
+              new_animal['customer_id'], ))
+
+        # The `lastrowid` property on the cursor will return
+        # the primary key of the last thing that got added to
+        # the database.
+        id = db_cursor.lastrowid
+
+        # Add the `id` property to the animal dictionary that
+        # was sent by the client so that the client sees the
+        # primary key in the response.
+        new_animal['id'] = id
+
+    return json.dumps(new_animal)
